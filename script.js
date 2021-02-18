0d4caf88457497c465385b894bb469d32d0322c6
anddreza <77905947+anddreza@users.noreply.github.com>
2021-02-17 20:47:02 -0400

    :package: formulário de orçamento

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 /*var hamburguer = document.querySelector(".hamburguer"); /*var, para selecionar o menu hamburguer*/
 
-document.querySelector(".hamburguer").addEventListener("click", () => /*adiconar um eventlistener para ouvir um evento de click*/
-    document.querySelector(".container").classList.toggle("show-menu") /* animação de dois elementos separados por isso o uso do container, ele envolve todo mundo*/
+/*document.querySelector(".hamburguer").addEventListener("click", () => /*adiconar um eventlistener para ouvir um evento de click*/
+    /*document.querySelector(".container").classList.toggle("show-menu") /* animação de dois elementos separados por isso o uso do container, ele envolve todo mundo*/
+
+document.querySelector(".hamburguer").addEventListener("click", () =>   
+    document.querySelector(".container").classList.toggle("show-menu")
+
 );
+
